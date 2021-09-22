6dfc0b5c1111e6f4d5d8edb6b2385e4c4b9d06fc
aMediocreDad <amediocredad@outlook.com>
2021-09-22 22:24:17 +0200

    :memo: Added example Rust and Svelte

---
+++ Examples/rust.rs
@@ -0,0 +1,45 @@
+use tokio::{
+  io::{AsyncBufReadExt, AsyncWriteExt, BufReader},
+  net::TcpListener,
+  sync::broadcast,
+};
+
+#[tokio::main]
+async fn main() {
+  let listener = TcpListener::bind("localhost:8080").await.unwrap();
+
+  let (tx, _rx) = broadcast::channel(10);
+
+  loop {
+      let (mut socket, addr) = listener.accept().await.unwrap();
+
+      let tx = tx.clone();
+      let mut rx = tx.subscribe();
+
+      tokio::spawn(async move {
+          let (read, mut writer) = socket.split();
+
+          let mut reader = BufReader::new(read);
+          let mut line = String::new();
+
+          loop {
+              tokio::select! {
+                  result = reader.read_line(&mut line) => {
+                      if result.unwrap() == 0 {
+                          break;
+                      }
+                      tx.send((line.clone(), addr)).unwrap();
+                      line.clear();
+                  }
+                  result = rx.recv() => {
+                      let (msg, other_addr) = result.unwrap();
+
+                      if addr != other_addr {
+                          writer.write_all(msg.as_bytes()).await.unwrap();
+                      }
+                  }
+              }
+          }
+      });
+  }
+}
